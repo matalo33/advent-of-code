c4b83016eb9551ca7f43f0b49decd32570b68788
Matthew Taylor <matthew@m-taylor.co.uk>
2020-05-12 17:00:44 +0100

    VSCode compatibility

--- 5/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/matalo33/advent-of-code
-
-go 1.13
--- 6/main_test.go
+++ 6/main_test.go
@@ -2,56 +2,56 @@ package main
 
 import "testing"
 
-var testData = []string {
-  "COM)B",
-  "B)C",
-  "C)D",
-  "D)E",
-  "E)F",
-  "B)G",
-  "G)H",
-  "D)I",
-  "E)J",
-  "J)K",
-  "K)L",
+var testData = []string{
+	"COM)B",
+	"B)C",
+	"C)D",
+	"D)E",
+	"E)F",
+	"B)G",
+	"G)H",
+	"D)I",
+	"E)J",
+	"J)K",
+	"K)L",
 }
 
-var orbitalTransferTestData = []string {
-  "COM)B",
-  "B)C",
-  "C)D",
-  "D)E",
-  "E)F",
-  "B)G",
-  "G)H",
-  "D)I",
-  "E)J",
-  "J)K",
-  "K)L",
-  "K)YOU",
-  "I)SAN",
+var orbitalTransferTestData = []string{
+	"COM)B",
+	"B)C",
+	"C)D",
+	"D)E",
+	"E)F",
+	"B)G",
+	"G)H",
+	"D)I",
+	"E)J",
+	"J)K",
+	"K)L",
+	"K)YOU",
+	"I)SAN",
 }
 
-func TestLoadData (t *testing.T) {
-  orbits := loadData(testData)
+func TestLoadData(t *testing.T) {
+	orbits := loadData(testData)
 
-  if len(orbits) != 11 {
-    t.Errorf("got %v want %v", len(orbits), 11)
-  }
+	if len(orbits) != 11 {
+		t.Errorf("got %v want %v", len(orbits), 11)
+	}
 }
 
 func TestCalculateOrbits(t *testing.T) {
-  orbits := loadData(testData)
+	orbits := loadData(testData)
 
-  if countOrbits(orbits) != 42 {
-    t.Errorf("got %v want %v", countOrbits(orbits), 42)
-  }
+	if countOrbits(orbits) != 42 {
+		t.Errorf("got %v want %v", countOrbits(orbits), 42)
+	}
 }
 
 func TestOrbitalTransfer(t *testing.T) {
-  orbits := loadData(orbitalTransferTestData)
+	orbits := loadData(orbitalTransferTestData)
 
-  if orbitalTransfer(orbits) != 4 {
-    t.Errorf("Got %v want %v", orbitalTransfer(orbits), 4)
-  }
+	if orbitalTransfer(orbits) != 4 {
+		t.Errorf("Got %v want %v", orbitalTransfer(orbits), 4)
+	}
 }
